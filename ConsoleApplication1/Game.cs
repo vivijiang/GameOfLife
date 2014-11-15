93f0aa7d2689a273877b504733ceccfa93836cde
vivi <mobojiang@gmail.com>
2014-11-15 15:41:17 +0800

    init

---
+++ ConsoleApplication1/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Game
+    {
+        public static int row;
+        public static int col;
+        public static int[,] Next(int[,] input)
+        {
+            row = input.GetLength(0);
+            col = input.GetLength(1);
+            int[,] result = new int[row, col];
+            ScanRow(input, result, 0, 0);
+            return result;
+        }
+
+        public static void ScanRow(int[,] source, int[,] result, int curRowIndex, int curColumnIndex)
+        {
+            if (curRowIndex == row)
+            {
+                return;
+                //ScanCurCell(source, result, curRowIndex, curColumnIndex);
+            }
+            else
+            {
+                ScanCurCell(source, result, curRowIndex, curColumnIndex);
+                curColumnIndex = (curColumnIndex + 1) % col;
+                if (curColumnIndex == 0) curRowIndex++;
+                ScanRow(source, result, curRowIndex, curColumnIndex);
+            }
+
+        }
+
+        public static int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1 };
+        public static int[] dy = { -1, 1, 0, 0, -1, 1, -1, 1 };
+        public static int[] alive = { 0, 0, 1, 1, 0, 0, 0, 0, 0 };
+        public static int[] dead = { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+
+        private static bool CheckIsInBound(int curRow, int curCol)
+        {
+            if (!(curRow >= 0 && curRow < row)) return false;
+            if (!(curCol >= 0 && curCol < col)) return false;
+            return true;
+        }
+
+        public static void ScanCurCell(int[,] source, int[,] result, int curRow, int curCol)
+        {
+            int count = 0;
+            if (CheckIsInBound(curRow + dx[0], curCol + dy[0])) count += source[curRow + dx[0], curCol + dy[0]];
+            if (CheckIsInBound(curRow + dx[1], curCol + dy[1])) count += source[curRow + dx[1], curCol + dy[1]];
+            if (CheckIsInBound(curRow + dx[2], curCol + dy[2])) count += source[curRow + dx[2], curCol + dy[2]];
+            if (CheckIsInBound(curRow + dx[3], curCol + dy[3])) count += source[curRow + dx[3], curCol + dy[3]];
+            if (CheckIsInBound(curRow + dx[4], curCol + dy[4])) count += source[curRow + dx[4], curCol + dy[4]];
+            if (CheckIsInBound(curRow + dx[5], curCol + dy[5])) count += source[curRow + dx[5], curCol + dy[5]];
+            if (CheckIsInBound(curRow + dx[6], curCol + dy[6])) count += source[curRow + dx[6], curCol + dy[6]];
+            if (CheckIsInBound(curRow + dx[7], curCol + dy[7])) count += source[curRow + dx[7], curCol + dy[7]];
+
+            if (source[curRow, curCol] == 1)
+                result[curRow, curCol] = alive[count];
+            else
+                result[curRow, curCol] = dead[count];
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[,] input = new int[,] { { 1 } };
+            var expect = new int[,] { { 0 } };
+            var actual = Game.Next(input);
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[,] input = new int[,] { { 1,1 } };
+            var expect = new int[,] { { 0 ,0} };
+            var actual = Game.Next(input);
+            CollectionAssert.AreEqual(expect, actual);
+        }
+    }
+}
